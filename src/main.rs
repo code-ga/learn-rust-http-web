17c7d739f5ac2f482c1a01109e8b3a398a871bbc
code-ga <tritranduc11@gmail.com>
2022-10-23 20:47:50 +0700

    update some things

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,10 @@ use std::net::TcpListener;
 
 fn main() {
     let mut handle = HttpHandler::new();
-    const PORT: i32 = 8000;
+    let port = match std::env::var("PORT") {
+        Ok(e) => e.parse::<i32>().unwrap(),
+        Err(_) => 8000,
+    };
 
     handle.get("/".to_owned(), home_handler);
     handle.all(log_request);
@@ -15,12 +18,12 @@ fn main() {
     new_route.get("/".to_owned(), home_handler);
 
     handle.route("/sub".to_string(), &new_route);
-    let listener = TcpListener::bind(format!("127.0.0.1:{PORT}"));
+    let listener = TcpListener::bind(format!("127.0.0.1:{port}"));
     match listener {
         Ok(listener) => {
             println!(
                 "máy chủ bật rồi đó ở cổng {} link đi đến đó nè http://{}",
-                PORT,
+                port,
                 listener.local_addr().unwrap()
             );
             handle.handle_http_request(listener);
@@ -31,10 +34,7 @@ fn main() {
     }
 }
 fn home_handler(_request: &mut HeaderData, response: &mut ResponseTool, _: &mut RouterManager) {
-    response.send(
-            "<h1>hello world</h1>".to_string(),
-        true,
-    );
+    response.send("<h1>hello world</h1>".to_string(), true);
 }
 fn log_request(request: &mut HeaderData, _response: &mut ResponseTool, _: &mut RouterManager) {
     println!("method: {}, path: {}", request.method, request.path);
